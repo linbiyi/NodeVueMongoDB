31dbee3e44b6f442c82ea6d4271a838889bc9559
l511407563 <511407563@qq.com>
2017-08-31 11:57:16 +0800

    123
    123

---
+++ demo3/demo3/server/app2.js
@@ -0,0 +1,38 @@
+// var mongoose = require('mongoose');
+// var PhotoList = mongoose.model('PhotoList');
+// module.exports = function(req, res, next) {
+// 	PhotoList.find({}, function(err, docs) {
+// 		if(err) {
+// 			res.end('Error');
+// 			return next();
+// 		}
+// 		res.send(JSON.stringify(docs));
+// 	});
+// }
+
+//mongo客户端
+var MongoClient = require('mongodb').MongoClient;
+//数据库
+var DB_TEST = 'mongodb://10.40.40.128:27017/test';
+//链接数据库
+var selectData = function(db, callback) {
+    //链接到表
+    var collection = db.collection('information');
+    //查询数据
+    collection.find().toArray(function(err, result) {
+        if(err) 
+        {
+            console.log("Error:"+ err);
+            return;
+        } 
+        callback(result);
+    });
+}
+
+MongoClient.connect(DB_TEST, (err, db) => {
+    console.log("链接成功");
+    selectData(db, function(result) {
+        console.log(result);
+        db.close();
+    });
+});
